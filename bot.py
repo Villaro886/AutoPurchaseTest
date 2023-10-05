e529aadae6db7ed5e6cecf7842fc755a13240179
Villaro886 <126690752+Villaro886@users.noreply.github.com>
2023-10-05 12:58:12 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 from discord.ext import tasks
 
 # log channel id, change the id in on_ready as well
-logs_channel_id = channelID
+logs_channel_id = 1120731249510723684
 
 
 intents = discord.Intents.default()
@@ -16,7 +16,7 @@ client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client) # for slash cmd
 
 # this cookie must be in the group, and have "configure avatar items" permissions
-cookie = ""
+cookie = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_153B87BD4819734FC43C0BDF3946242E50BC497338FAA41DBA0BF9518363F58A2515BDC4810A436E058F034CC7EA045FB398B245A4D5C7538F3EC01A9651F0B788EE6B14BEED8E90C639B1F5DD9345515EA34C3D9C2822A28C3FD22A6456282D39141DB4D16114E617FEFEA4178A2556612BC12F9C8DD3BEB99EC7EF295B24D01251DF986CD7BFFA85D75399065E4B9404995A68287EB3E5D1726236EE6B79248C427DA7E6CB76E00941DB8A184080BBC473842AF50CB07DF0D426DAF01B74AE44C40610778718A4C31A9EBE9907BB53BA290A0B2483A172A4C8A28991DE09530CD69DA24927EC8AB9823214C43F606F4C0A44CECD212CFFECFDDF252BAAA6D7F2C8C98D7154F0F4407B516BBF333A94D568D125A54C6E253203E4E330ACB1FC2A8D9B2D09E188568253E1D10063C00DAFE5876FF2B7A138AFD3865366A095DF9ECCE7EDAEA8BCC5E8C93965AD7AE42F5A09C983574736005DF6C71A88007187CF3A84762B4CF34D5405E4B5E19EE3796A33571B8B96C4CC922BB1968CD5FD3DFDAD21D94AAB72C73DE7A59F060413E06191F4976AF0E12363387DEB836B3FC5D90F2E744DED2DD75EEFF95B93C6D79A7E9F128B39D585F6C2E93B4336BF7693619AA065EF6BA5F9495873099CC8A5B68C58BC70C28F6D82E6F5EA24E377540BB383CB7E310C2D42DE5547574FA70409C7122DB59A3950CC8161FE3BC63046D11CC675E2E8C5DE70D355095560AFB15F94FA19EC8A2CE53559BA6C0C864967AC7E3C59AA49F5B3915EF4CAF9AB8F9CABC6A551AB6FA22371C9350CD9C0F9C5B63D661B8F6C4DE3BFB4F6FB17ACD5DAF63C95C73B89D43369CF86D5640FBA10D3441EBFB937D5B9FD34DAE302F9F0C526A60F7F5285F69E02FDBAD9EA70BC5989AAF2D5CA9DD14272F80720FA55779A9E425943B7191E5072DFA7DC6915EE990EBCA9E4FDBBA720D6CBCACEF40289A7CF070184C332CA57904B1BD7989FDCF16D3D3F58C92F3952C8626C7D12F02CCA01572E1D772D03C0B4A009BDBE1018452760E8497B676DAB97BD81BC4DCDEC812744064318"
 # to check if purchase id has been used (purchase ids get logged to prevent bypassing)
 def check_purchase_id(purchase_id):
     with open("usedbuys.txt", "r") as file:
@@ -100,39 +100,39 @@ def get_user_id(username):
 @tree.command(name="purchase", description="Command to upgrade to premium/bonus!", guild=discord.Object(id=guildID)) # guild ID
 @app_commands.describe(packages='Packages to choose')
 @app_commands.choices(packages=[
-    discord.app_commands.Choice(name='1 Week Bonus', value=13456613769),
-    discord.app_commands.Choice(name='1 Day Premium', value=13456615376),
-    discord.app_commands.Choice(name='1 Week Premium', value=13456616286),
-    discord.app_commands.Choice(name='2 Week Premium', value=13456614396),
+    discord.app_commands.Choice(name='1 Week Bonus', value=892800303),
+    discord.app_commands.Choice(name='1 Day Premium', value=892800460),
+    discord.app_commands.Choice(name='1 Week Premium', value=868427833),
+    discord.app_commands.Choice(name='2 Week Premium', value=931774771),
 ])
 
 async def purchase_command(interaction, packages: discord.app_commands.Choice[int]):
-    channel_id = 1117516685944037487  # channel you want the command ran in
+    channel_id = 1155327783904497715  # channel you want the command ran in
     if interaction.channel_id != channel_id:
         return
     user = interaction.user
  # roles for each package/value. make sure you change the role id and value to corrospond correctly
  # easily can rework to not set a duration, it will be more stable as well since you dont have to check for expired roles
     package_roles = {
-        13456613769: {
+        892800303: {
             'role_id': 1107749003648577566,
             'duration': timedelta(weeks=1)
         },
-        13456615376: {
+        892800460: {
             'role_id': 1107746543693463563,
             'duration': timedelta(days=1)
         },
-        13456616286: {
+        868427833: {
             'role_id': 1107746543693463563,
             'duration': timedelta(weeks=1)
         },
-        13456614396: {
+        931774771: {
             'role_id': 1107746543693463563,
             'duration': timedelta(weeks=2)
         }
     }
 
-    logs_channel = client.get_channel(logs_channel_id)
+    logs_channel = client.get_channel(1159345574412296274)
     log_message = discord.Embed(
         title="Purchase Command",
         description=f"Purchase command used\nUser: `{user.name}#{user.discriminator}`\nOption: `{packages.name}`",
@@ -141,10 +141,10 @@ async def purchase_command(interaction, packages: discord.app_commands.Choice[in
     await logs_channel.send(embed=log_message)
 
     ## change with the correct urls. 
-    url_1_week_bonus = "https://www.roblox.com/catalog/13456613769/Bonus"
-    url_1_day_premium = "https://www.roblox.com/catalog/13456615376/Prem1DAY"
-    url_1_week_premium = "https://www.roblox.com/catalog/13456616286/Premium"
-    url_2_week_premium = "https://www.roblox.com/catalog/13456614396/Prem2Week"
+    url_1_week_bonus = "https://www.roblox.com/catalog/892800303/1-Week-Membership-Bonus"
+    url_1_day_premium = "https://www.roblox.com/catalog/892800460/1-Day-Membership"
+    url_1_week_premium = "https://www.roblox.com/catalog/868427833/1-Week-Membership"
+    url_2_week_premium = "https://www.roblox.com/catalog/931774771/2-Weeks-Membership"
 
     embed_channel = discord.Embed(
         title="DM Sent",
@@ -160,13 +160,13 @@ async def purchase_command(interaction, packages: discord.app_commands.Choice[in
     asyncio.create_task(delete_message())  
 
     # make sure you change the values to the corrosponding ones above
-    if packages.value == 13456613769:
+    if packages.value == 892800303:
         url = url_1_week_bonus
-    elif packages.value == 13456615376:
+    elif packages.value == 892800460:
         url = url_1_day_premium
-    elif packages.value == 13456616286:
+    elif packages.value == 868427833:
         url = url_1_week_premium
-    elif packages.value == 13456614396:
+    elif packages.value == 931774771:
         url = url_2_week_premium
     else:
         url = ""
@@ -371,7 +371,7 @@ async def on_ready():
     await tree.sync(guild=discord.Object(id=guildID)) # guild ID
     print("Ready!")
 
-    logs_channel_id = channelID # log channel ID
+    logs_channel_id = 1159345574412296274 # log channel ID
     logs_channel = client.get_channel(logs_channel_id)
     log_message = "Bot ready - commands loaded"
     await logs_channel.send(log_message)
@@ -392,4 +392,4 @@ async def on_ready():
     print("Bots ready - Roles configured")
 
 ## discord token
-client.run("token here")
+client.run("MTE1OTM1MzM2MzUzMjYyMzkxNA.Gq0Dn_.pnClnvBJO3apW45ZT8jNcdF_KDzPvryNNUPKmg")
